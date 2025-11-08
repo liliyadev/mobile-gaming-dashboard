33330753afb531a6bdc6ea0aa08859cf08a9edaf
Liliya Vildanova <liliya.vild@gmail.com>
2025-11-07 19:52:17 -0500

    Fix build errors and enable standalone components

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { GameService } from './app/services/game.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    GameService, 
   ],
 });
